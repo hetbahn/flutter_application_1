2ac7e4e7f7bea28880e9bdf8e5c3dd9a75c454ae
summeracne <summeracne@gmail.com>
2021-01-21 17:44:38 +0900

    이미지에 별점주기

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     ));
 
@@ -12,8 +13,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   PageController _pageController;
 
-  void _onScroll() {
-  }
+  void _onScroll() {}
 
   @override
   void initState() {
@@ -30,21 +30,133 @@ class _HomePageState extends State<HomePage> {
       body: PageView(
         controller: _pageController,
         children: <Widget>[
-          makePage(),
-          makePage(),
-          makePage(),
+          makePage(image: 'assets/images/one.jpg', title: '', description: ''),
+          makePage(image: 'assets/images/two.jpg', title: '', description: ''),
+          makePage(
+              image: 'assets/images/three.jpg', title: '', description: ''),
+          makePage(image: 'assets/images/four.jpg', title: '', description: ''),
         ],
       ),
     );
   }
 }
 
-Widget makePage() {
+Widget makePage({image, title}) {
   return Container(
     decoration: BoxDecoration(
-      image: DecorationImage(
-        image: AssetImage('assets/images/one.jpg')
-      )
+        image: DecorationImage(
+            image: AssetImage('assets/images/one.jpg'), fit: BoxFit.cover)),
+    child: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(begin: Alignment.bottomRight, stops: [
+        0.3,
+        0.9
+      ], colors: [
+        Colors.black.withOpacity(.9),
+        Colors.black.withOpacity(.2),
+      ])),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 40,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: <Widget>[
+                  Text(
+                    '1',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '/4',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  )
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 3),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.yellow,
+                            size: 15,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(right: 5),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.grey,
+                            size: 15,
+                          ),
+                        ),
+                        Text(
+                          '4.0',
+                          style: TextStyle(color: Colors.white70),
+                        ),
+                        Text(
+                          '(2300)',
+                          style: TextStyle(color: Colors.white38, fontSize: 12),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(description)
+                  ],
+                ),
+              )
+            ]),
+      ),
     ),
   );
 }
